61adf76b2228c6bb4ad6dad959a95e6772f34961
Sharraff <rogandsteinporter@gmail.com>
2024-01-16 11:15:53 -0800

    Reddit API pagination

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and return the number of 
-subcribers for s given subredit
 """
-
+function that queries the Reddit API and return the number of
+subcribers for a given subredit
+"""
 import requests
 import json
 
 
-
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and return the number of 
+    function that queries the Reddit API and return the number of
     subcribers for s given subredit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
